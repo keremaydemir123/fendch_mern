8b02acdffbcad5eeb638f0a8cba80601e35710d5
Kerem Aydemir <keremaydemir59@gmail.com>
2022-11-29 23:47:26 +0300

    challenge card is added to storybook

---
+++ client/src/stories/ChallengeCard.stories.tsx
@@ -0,0 +1,9 @@
+import ChallengeCard from './ChallengeCard';
+
+export default {
+  title: 'Challenge Card',
+  component: ChallengeCard,
+};
+
+// example video: https://www.youtube.com/watch?v=FUKpWgRyPlU
+export const Default = () => <ChallengeCard />;
