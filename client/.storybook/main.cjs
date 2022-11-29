75258d9cb76218bcefc6f73a026fadec2ae58eb3
Kerem Aydemir <keremaydemir59@gmail.com>
2022-11-29 19:55:09 +0300

    storybook setup for project card

--- client/.storybook/main.cjs
+++ client/.storybook/main.cjs
@@ -1,19 +1,25 @@
 const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+    // End PostCSS
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "docs": {
-    "docsPage": true
-  }
-}
+  docs: {
+    docsPage: true,
+  },
+};
---
+++ client/src/stories/ProjectCard.tsx
@@ -0,0 +1,23 @@
+function ProjectCard({ layout = 'default' }) {
+  return (
+    <div>
+      {layout === 'default' && (
+        <div className="bg-red w-64 h-64" style={{ background: 'red' }}>
+          <h1>Project Card</h1>
+        </div>
+      )}
+      {layout === 'grid' && (
+        <div className="bg-red w-32 h-32">
+          <h1>Project Card</h1>
+        </div>
+      )}
+      {layout === 'list' && (
+        <div className="bg-red w-64 h-16">
+          <h1>Project Card</h1>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProjectCard;
